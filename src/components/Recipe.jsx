618718dbbb27e92f47c9a7950cec94114644f114
diaz-camilo <diaz.camilo@icloud.com>
2021-12-16 18:49:18 +1100

    recipe details extracted and rendered from json

--- src/components/Recipe.jsx
+++ src/components/Recipe.jsx
@@ -1,59 +1,59 @@
 import { Link } from "react-router-dom";
 import styled from "styled-components";
 
+const Tags = styled.div`
+  color: hsla(0, 0%, 100%);
+  position: absolute;
+  display: none;
+  flex-wrap: wrap;
+  gap: 0.25vw;
+  padding: 1.5vw 0.25vw 0.25vw 0.25vw;
+  bottom: 0;
+  background-image: linear-gradient(
+    hsla(0, 0%, 100%, 0),
+    hsla(0, 0%, 100%, 0.9)
+  );
+`;
+
+const RecipeCard = styled.div`
+  width: 400px;
+  height: 400px;
+  background-image: url(${(props) => props.bgImg});
+  background-repeat: no-repeat;
+  background-size: cover;
+  background-position: center;
+  position: relative;
+  box-shadow: 10px 10px 30px hsl(0, 0%, 25%);
+
+  &:hover {
+    box-shadow: 10px 10px 30px hsl(90, 100%, 24%);
+  }
+
+  &:hover ${Tags} {
+    display: flex;
+  }
+`;
+
+const Header = styled.div`
+  background-image: linear-gradient(hsl(0, 0%, 0%, 0.9), hsl(0, 0%, 0%, 0));
+  color: hsl(0, 100%, 100%);
+  margin: 0;
+  padding-top: 1vw;
+  padding-bottom: 2vw;
+`;
+
+const Tag = styled.span`
+  background-color: lightcoral;
+  border-radius: 00.25vw;
+  padding: 00.25vw;
+`;
+
 export default function Recipe(props) {
   const { thumbnail_url, credits, name, tags, id } = props.recipe;
 
-  const Tags = styled.div`
-    color: hsla(0, 0%, 100%);
-    position: absolute;
-    display: none;
-    flex-wrap: wrap;
-    gap: 0.25vw;
-    padding: 1.5vw 0.25vw 0.25vw 0.25vw;
-    bottom: 0;
-    background-image: linear-gradient(
-      hsla(0, 0%, 100%, 0),
-      hsla(0, 0%, 100%, 0.9)
-    );
-  `;
-
-  const RecipeCard = styled.div`
-    width: 400px;
-    height: 400px;
-    background-image: ${`url(${thumbnail_url})`};
-    background-repeat: no-repeat;
-    background-size: cover;
-    background-position: center;
-    position: relative;
-    box-shadow: 10px 10px 30px hsl(0, 0%, 25%);
-
-    &:hover {
-      box-shadow: 10px 10px 30px hsl(90, 100%, 24%);
-    }
-
-    &:hover ${Tags} {
-      display: flex;
-    }
-  `;
-
-  const Header = styled.div`
-    background-image: linear-gradient(hsl(0, 0%, 0%, 0.9), hsl(0, 0%, 0%, 0));
-    color: hsl(0, 100%, 100%);
-    margin: 0;
-    padding-top: 1vw;
-    padding-bottom: 2vw;
-  `;
-
-  const Tag = styled.span`
-    background-color: lightcoral;
-    border-radius: 00.25vw;
-    padding: 00.25vw;
-  `;
-
   return (
     <Link to={`/details/${id}`}>
-      <RecipeCard>
+      <RecipeCard bgImg={thumbnail_url}>
         <Header>
           <h2>{name}</h2>
           <h4>by: {credits[0].name}</h4>
--- src/components/RecipeDetails.jsx
+++ src/components/RecipeDetails.jsx
@@ -9,8 +9,8 @@ const HEADERS = {
 const BASE_URL = "https://tasty.p.rapidapi.com/";
 
 export default function RecipeDetails(props) {
+  debugger;
   const params = useParams();
-
   const [recipe, setRecipe] = useState({});
   const [isLoaded, setIsLoaded] = useState(false);
   const [error, setError] = useState(null);
@@ -20,13 +20,24 @@ export default function RecipeDetails(props) {
     description,
     num_servings,
     credits,
-    coock_time_minutes,
+    cook_time_minutes,
     instructions,
     nutrition,
     thumbnail_url,
     thumbnail_alt_text,
     id,
+    original_video_url,
+    user_ratings,
   } = recipe;
+
+  // in case instrucions do not come in order
+  let sortedInstructions = [];
+  if (isLoaded) {
+    sortedInstructions = instructions.slice();
+    sortedInstructions.sort((a, b) => a.position - b.position);
+  }
+
+  // console.log("sorted instrucions", instructions);
 
   const options = {
     method: "GET",
@@ -39,8 +50,8 @@ export default function RecipeDetails(props) {
     axios
       .request(options)
       .then((response) => {
-        setIsLoaded(true);
         setRecipe(response.data);
+        setIsLoaded(true);
         console.log(response.data);
       })
       .catch(function (error) {
@@ -52,5 +63,65 @@ export default function RecipeDetails(props) {
     return <p>Loading...</p>;
   } else if (error) {
     return <p>{error}</p>;
-  } else return <h1>details for {id}</h1>;
+  } else {
+    return (
+      <div>
+        <h1>{name}</h1>
+        <h2>By: {credits.map((auth) => auth.name + ", ")}</h2>
+        <h3>
+          Servings: {num_servings} | {cook_time_minutes} minutes
+        </h3>
+        <h3>
+          Up votes: {user_ratings.count_positive} | Down votes:{" "}
+          {user_ratings.count_negative} | Rating:{" "}
+          {Math.round(user_ratings.score * 100)}%
+        </h3>
+        <video controls width="480" src={original_video_url}></video>
+        <p>Description: {description}</p>
+        <div className="instructions">
+          <h3>Instructions</h3>
+          {instructions.map((ins) => (
+            <>
+              <p key={ins.id}>
+                <strong>Step {ins.position}</strong>
+                <br />
+                {ins.display_text}
+              </p>
+              <br />
+            </>
+          ))}
+        </div>
+        <table>
+          <thead>
+            <tr>
+              <th>Calories</th>
+              <th>{nutrition.calories}</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>carbohydrates</td>
+              <td>{nutrition.carbohydrates}</td>
+            </tr>
+            <tr>
+              <td>sugar</td>
+              <td>{nutrition.sugar}</td>
+            </tr>
+            <tr>
+              <td>fat</td>
+              <td>{nutrition.fat}</td>
+            </tr>
+            <tr>
+              <td>protein</td>
+              <td>{nutrition.protein}</td>
+            </tr>
+            <tr>
+              <td>fiber</td>
+              <td>{nutrition.fiber}</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    );
+  }
 }
